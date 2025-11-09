60a91970b1139ebb33539d22d5d2d2e82a09d0ea
David English <dwenglish97@outlook.com>
2025-11-09 17:42:27 -0500

    clean up

--- src/cleaning.py
+++ src/cleaning.py
@@ -5,6 +5,7 @@ class DataCleaner:
     def __init__(self, df):
         self.df = df.copy()
     
+    # drops rows where there is found an '(n)' value
     def drop_n_values(self):
         column_names = self.df.columns
         for column in column_names:
@@ -12,15 +13,16 @@ class DataCleaner:
                 mask = self.df[column].str.contains(r'\(n\)', na=False)
                 self.df = self.df[~mask]
             else:
-                pass  
-               
+                pass     
         return self
         
-        
+    # renames columns in unemployment dataset    
     def rename_emp(self):
         self.df.rename(columns={
         "Area FIPS Code" : "FIPS Code",
         "ST FIPS Code" : "State FIPS Code",
         }, inplace=True)
         return self
-    # def rename_house(self): 
+
+    def cols_to_int64(self,df):
+        
